8f32d5c62b28cc6999b5f5edf93427669b045a5e
Toby Law <toby@tzcl.me>
2021-09-04 18:38:02 +1000

    Added functions and error recovery

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+import Control.Monad.Error
 import Control.Monad.State
 import qualified Data.Map as M
 import Text.Parsec
@@ -15,11 +16,16 @@ data Expr
   | Mod Expr Expr
   | Add Expr Expr
   | Sub Expr Expr
+  | Function String Expr
+  deriving (Show)
+
+data FunctionBody = FunctionBody String Expr
   deriving (Show)
 
 data Statement
   = Print Expr
-  | Assign String
+  | Assignment String Expr
+  | Definition String FunctionBody
   deriving (Show)
 
 lexer :: TokenParser ()
@@ -59,31 +65,57 @@ parseExpr =
     ]
     parseTerm
 
+parseFunction :: Parser Expr
+parseFunction = do
+  ident <- identifier lexer
+  expr <- parens lexer parseExpr
+  return $ Function ident expr
+
 parseTerm :: Parser Expr
-parseTerm = parens lexer parseExpr <|> parseNumber <|> parseName
+parseTerm = parens lexer parseExpr <|> parseNumber <|> try parseFunction <|> parseName
 
 parsePrint :: Parser Statement
 parsePrint = do
   reserved lexer "print"
   Print <$> parseExpr
 
+parseAssignment :: Parser Statement
+parseAssignment = do
+  reserved lexer "let"
+  ident <- identifier lexer
+  reservedOp lexer "="
+  Assignment ident <$> parseExpr
+
+parseDefinition :: Parser Statement
+parseDefinition = do
+  reserved lexer "def"
+  ident <- identifier lexer
+  argname <- parens lexer $ identifier lexer
+  reservedOp lexer "="
+  Definition ident . FunctionBody argname <$> parseExpr
+
 parseInput :: Parser Statement
 parseInput = do
   whiteSpace lexer
-  s <- parsePrint
+  s <- parsePrint <|> parseAssignment <|> parseDefinition
   eof
   return s
 
 -- Interpreter
-type Calculator a = StateT (M.Map String Expr) IO a
+type StoredVal = Either Double FunctionBody
 
-interpretExpr :: Expr -> Calculator Double
+type Calculator = StateT (M.Map String StoredVal) IO
+
+type SafeCalculator a = ErrorT String Calculator a
+
+interpretExpr :: Expr -> SafeCalculator Double
 interpretExpr (Const n) = return n
 interpretExpr (Name n) = do
   varmap <- get
   case M.lookup n varmap of
-    Nothing -> fail $ "Uknown identifier: " ++ n
-    Just e -> interpretExpr e
+    Nothing -> fail $ "Unknown identifier: " ++ n
+    Just (Left n) -> return n
+    Just (Right _) -> fail ("You must call function " ++ n)
 interpretExpr (Neg expr) = do
   val <- interpretExpr expr
   return $ negate val
@@ -110,25 +142,49 @@ interpretExpr (Sub e1 e2) = do
   v1 <- interpretExpr e1
   v2 <- interpretExpr e2
   return $ v1 - v2
+interpretExpr (Function fn e) = do
+  context <- get
+  case M.lookup fn context of
+    Nothing -> fail ("Unknown function: " ++ fn)
+    Just (Left _) -> fail ("Cannot call constant: " ++ fn)
+    Just (Right (FunctionBody argname expr)) -> do
+      n <- interpretExpr e
+      modify (M.insert argname (Left n))
+      r <- interpretExpr expr
+      put context
+      return r
 
-interpretStatement :: Statement -> Calculator ()
+interpretStatement :: Statement -> SafeCalculator ()
 interpretStatement (Print expr) = do
   n <- interpretExpr expr
   liftIO $ print n
+interpretStatement (Assignment ident expr) = do
+  n <- interpretExpr expr
+  modify (M.insert ident (Left n))
+interpretStatement (Definition fn body) = do
+  modify (M.insert fn (Right body))
 
-defaultVars :: M.Map String Expr
+defaultVars :: M.Map String StoredVal
 defaultVars =
   M.fromList
-    [ ("pi", Const 3.14)
+    [ ("pi", Left 3.14)
     ]
 
-calculate :: String -> IO ()
+calculate :: String -> Calculator ()
 calculate s =
   case ret of
-    Left e -> putStrLn $ "error: " ++ show e
-    Right n -> evalStateT (interpretStatement n) defaultVars
+    Left e -> liftIO $ putStrLn $ "error: " ++ show e
+    Right n -> do
+      res <- runErrorT $ interpretStatement n
+      case res of
+        Left e' -> liftIO $ putStrLn $ "run error: " ++ e'
+        Right _ -> return ()
   where
     ret = parse parseInput "calculator" s
 
+calculator :: Calculator ()
+calculator =
+  liftIO getContents >>= mapM_ calculate . lines
+
 main :: IO ()
-main = getContents >>= mapM_ calculate . lines
+main = evalStateT calculator defaultVars
